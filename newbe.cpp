c2a75f0b3483a886bcbcd8465f8d4ed68dcb0e6e
naomimt <nmesfin13@gmail.com>
2025-03-15 00:44:20 +0300

    added a new cpp

--- newbe.cpp
+++ newbe.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
 
-int main() 
-{
-    std::cout<< "Hellow world";
+int main() {
+    int x,y;
+    cout<<" please enter 2  numbers ";
+    cin>>x>>y;
+    int sum= x+y;
+    cout<< "the sum is "<<sum<<endl;
+    return 0;
 }
